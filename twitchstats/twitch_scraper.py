cf74c5f45c6546baf8466453f1bc613a19cb117b
RowanMeara <rrm89@cornell.edu>
2017-06-13 21:28:20 -0500

    Added Riot's English language twitch channels to the config file.

--- twitchstats/twitch_scraper.py
+++ twitchstats/twitch_scraper.py
@@ -4,6 +4,8 @@ import json
 import time
 from pymongo import MongoClient
 
+DEBUG = False
+
 
 class TwitchScraper:
     """
@@ -14,7 +16,7 @@ class TwitchScraper:
         with open('twitch_config.yml') as f:
             config = yaml.load(f)
             self.db_port = config['db']['port']
-            self.db_location = config['db']['location']
+            self.db_host = config['db']['host']
             self.top_games_url = config['api']['top_games']
             self.api_version_url = config['api']['version']
         with open('../keys.yml') as f:
@@ -29,7 +31,7 @@ class TwitchScraper:
         self.session.headers.update(self.client_header)
         self.session.headers.update(self.api_version_header)
 
-    def top_games(self):
+    def scrape_top_games(self):
         """
         Retrieves and stores the current viewership and number of broadcasting channels
         for each of the top 100 games by viewer count.  Results are stored in a MongoDB collection.
@@ -38,24 +40,37 @@ class TwitchScraper:
         """
         api_result = self.session.get(self.top_games_url)
 
-        db_entry = {'timestamp' : int(time.time())}
+        db_entry = {'timestamp': int(time.time())}
         games, json_result = {}, json.loads(api_result.text)
         for game in json_result['top']:
-            games[str(game['game']['giantbomb_id'])]  = {
-                'name' : game['game']['name'],
-                'viewers' : game['viewers'],
-                'channels' : game['channels']
+            games[str(game['game']['giantbomb_id'])] = {
+                'name': game['game']['name'],
+                'viewers': game['viewers'],
+                'channels': game['channels']
             }
         db_entry['games'] = games
 
-        db = MongoClient(self.db_location, self.db_port).twitch_stats
+        db = MongoClient(self.db_host, self.db_port).twitch_stats
         collection = db.top_games
         db_result = collection.insert_one(db_entry)
-        print(db_result.inserted_id)
+        if DEBUG:
+            print(db_result.inserted_id)
+
+    def scrape_esports_channels(self, game):
+        """
+        Scrapes viewership data from all of the twitch channels that are designated as esports channels for that game.
+        If the a channel is not live or playing a different game, that channels results are not stored.
+
+        :param game: str name of the desired games, must match the one in the config file.
+        :return:
+        """
+
 
 if __name__ == "__main__":
     a = TwitchScraper()
     while True:
-        a.top_games()
-        time.sleep(300)
-    print("Done.")
+        start_time = time.time()
+        a.scrape_top_games()
+        if DEBUG:
+            print("Elapsed time: {:2f}".format(time.time() - start_time))
+        time.sleep(300 - (time.time() - start_time))
