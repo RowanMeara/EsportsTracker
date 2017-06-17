145fc66384378f711845e3ad7a4b09db5f138eba
RowanMeara <rrm89@cornell.edu>
2017-06-16 23:04:22 -0500

    Implemented a scraping function for esports channels.

--- twitchstats/twitch_scraper.py
+++ twitchstats/twitch_scraper.py
@@ -2,23 +2,24 @@ import yaml
 import requests
 import json
 import time
+import urllib
 from pymongo import MongoClient
 
-DEBUG = False
-
+DEBUG = True
 
 class TwitchScraper:
     """
     Makes twitch api calls and stores partially filtered results in a MongoDB collection.
     """
-
     def __init__(self):
         with open('twitch_config.yml') as f:
             config = yaml.load(f)
             self.db_port = config['db']['port']
             self.db_host = config['db']['host']
             self.top_games_url = config['api']['top_games']
+            self.live_streams_url = config['api']['live_streams']
             self.api_version_url = config['api']['version']
+            self.esports_channels = config['esports_channels']
         with open('../keys.yml') as f:
             keys = yaml.load(f)
             self.client_id = keys['twitchclientid']
@@ -36,7 +37,7 @@ class TwitchScraper:
         Retrieves and stores the current viewership and number of broadcasting channels
         for each of the top 100 games by viewer count.  Results are stored in a MongoDB collection.
 
-        :return: True
+        :return:
         """
         api_result = self.session.get(self.top_games_url)
 
@@ -44,8 +45,8 @@ class TwitchScraper:
         games, json_result = {}, json.loads(api_result.text)
         for game in json_result['top']:
             games[str(game['game']['giantbomb_id'])] = {
-                'name': game['game']['name'],
-                'viewers': game['viewers'],
+                'name':     game['game']['name'],
+                'viewers':  game['viewers'],
                 'channels': game['channels']
             }
         db_entry['games'] = games
@@ -59,18 +60,47 @@ class TwitchScraper:
     def scrape_esports_channels(self, game):
         """
         Scrapes viewership data from all of the twitch channels that are designated as esports channels for that game.
-        If the a channel is not live or playing a different game, that channels results are not stored.
+        If the a channel is not live or playing a different game, that channels results are not stored.  Requests are
+        issued synchronously due to twitch's rate limit.  Assume that if an esports channel is live, it is among the
+        current 100 most popular channels for its respective game.
 
-        :param game: str name of the desired games, must match the one in the config file.
+        :param game: str: name of the desired game, see config file
         :return:
         """
+        with open('twitch_config.yml') as f:
+            channel_list = yaml.load(f)['esports_channels'][game]
+        api_result = self.session.get(self.live_streams_url.format(game))
+        if DEBUG:
+            print(api_result.text)
+
+        db_entry = {'timestamp': int(time.time()), 'game': game}
+        streams, json_result = {}, json.loads(api_result.text)
+        for stream in json_result['streams']:
+            if not stream['channel']['display_name'].lower() in self.esports_channels[game]:
+                pass
+
+            # Recording the id twice in preparation for moving to a pure broadcaster id format.
+            streams[str(stream['channel']['_id'])] = {
+                'display_name':     stream['channel']['display_name'],
+                'viewers':          stream['viewers'],
+                'game':             stream['game'],
+                'status':           stream['channel']['status'],
+                'broadcaster_id':   stream['channel']['_id']
+            }
+        db_entry['streams'] = streams
+        db = MongoClient(self.db_host, self.db_port).twitch_stats
+        collection = db.esports_streams
+        db_result = collection.insert_one(db_entry)
+        if DEBUG:
+            print(db_result.inserted_id)
 
 
 if __name__ == "__main__":
     a = TwitchScraper()
     while True:
         start_time = time.time()
-        a.scrape_top_games()
+        # a.scrape_top_games()
+        a.scrape_esports_channels('League of Legends')
         if DEBUG:
-            print("Elapsed time: {:2f}".format(time.time() - start_time))
+            print("Elapsed time: {:.2f}s".format(time.time() - start_time))
         time.sleep(300 - (time.time() - start_time))
