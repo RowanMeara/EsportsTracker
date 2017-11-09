e9a8fcb3ae44a69055796130bdfc4044dfde93c3
RowanMeara <rrm89@cornell.edu>
2017-11-09 00:33:13 -0800

    Beginning the improved frontend design.

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -27,10 +27,10 @@ module.exports = {
       },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000000&mimetype=application/font-woff'
+        loader: 'url-loader?limit=1000000000&mimetype=application/font-woff'
       },
       { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'base64-inline-loader?limit=1000000&name=[name].[ext]' }
+        loader: 'base64-inline-loader?limit=1000000000&name=[name].[ext]' }
     ]
   },
   plugins: [
